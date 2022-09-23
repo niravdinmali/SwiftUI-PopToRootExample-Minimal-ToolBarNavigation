2e0e8d2cfa61e78b712404ac1344e4ba81145c25
webrichsoftware <eknath@webrichsoftware.com>
2022-09-23 13:26:05 +0530

    changed to toolbar navigation removed unessential code

--- SwiftUI-PopToRootExample/ContentView.swift
+++ SwiftUI-PopToRootExample/ContentView.swift
@@ -1,84 +1,47 @@
-//
-//  ContentView.swift
-//  SwiftUI-PopToRootExample
-//
-//  Created by Chuck Hartman on 5/20/20.
-//  Copyright © 2020 ForeTheGreen. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isActive : Bool = false
-    var body: some View {
-        NavigationView {
-            VStack {
-                Text("Root")
-                NavigationLink(destination: ContentView2(), isActive: self.$isActive )
-                { Text("Push") }
-                    .isDetailLink(false)
-            }
-            .navigationBarTitle("Root")
-        }
-        .navigationViewStyle(StackNavigationViewStyle())
-        .environment(\.rootPresentationMode, self.$isActive)
-    }
+   @State private var isActive : Bool = false
+   var body: some View {
+       NavigationView {
+           Text("Welcome")
+               .toolbar {
+                   NavigationLink(destination: ContentView2(), isActive: self.$isActive )
+                   {Label("", systemImage: "plus")                    }
+               }
+       }
+       .navigationViewStyle(StackNavigationViewStyle())
+       .environment(\.rootPresentationMode, self.$isActive)
+   }
 }
 
 struct ContentView2: View {
-    @State private var isActive : Bool = false
-    @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
-    @Environment(\.rootPresentationMode) private var rootPresentationMode: Binding<RootPresentationMode>
-    var body: some View {
-        VStack {
-            Text("Two")
-            NavigationLink(destination: ContentView3(), isActive: self.$isActive)
-            { Text("Push") }
-                .isDetailLink(false)
-            Button (action: { self.presentationMode.wrappedValue.dismiss() } )
-            { Text("Pop") }
-            Button (action: { self.rootPresentationMode.wrappedValue.dismiss() } )
-            { Text("Pop to root") }
-        }
-        .navigationBarTitle("Two")
-    }
+   @Environment(\.rootPresentationMode) private var rootPresentationMode: Binding<RootPresentationMode>
+   var body: some View {
+       VStack {
+           Text("First View")
+           NavigationLink(destination: ContentView3())
+           {Text("Go to Next View")}
+       }
+       .navigationBarTitle("First View")
+   }
 }
 struct ContentView3: View {
-    @State private var isActive : Bool = false
-    @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
-    @Environment(\.rootPresentationMode) private var rootPresentationMode: Binding<RootPresentationMode>
-    var body: some View {
-        VStack {
-            Text("Three")
-            NavigationLink(destination: ContentView4(), isActive: self.$isActive)
-            { Text("Push") }
-                .isDetailLink(false)
-            Button (action: { self.presentationMode.wrappedValue.dismiss() } )
-            { Text("Pop") }
-            Button (action: { self.rootPresentationMode.wrappedValue.dismiss() } )
-            { Text("Pop to root") }
-        }
-        .navigationBarTitle("Three")
-    }
+   @Environment(\.rootPresentationMode) private var rootPresentationMode: Binding<RootPresentationMode>
+   var body: some View {
+       VStack {
+           Text("Last View")
+           Button (action: { self.rootPresentationMode.wrappedValue.dismiss() } )
+           { Text("Pop to root") }
+       }
+       .navigationBarTitle("Last View")
+   }
 }
 
-struct ContentView4: View {
-    @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
-    @Environment(\.rootPresentationMode) private var rootPresentationMode: Binding<RootPresentationMode>
-    var body: some View {
-        VStack {
-            Text("Four")
-            Button (action: { self.presentationMode.wrappedValue.dismiss() } )
-            { Text("Pop") }
-            Button (action: { self.rootPresentationMode.wrappedValue.dismiss() } )
-            { Text("Pop to root") }
-        }
-        .navigationBarTitle("Four")
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+       ContentView()
+   }
 }
